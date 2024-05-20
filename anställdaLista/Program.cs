d9528a53a6184e9b10a7b8857e36b3b1a5ee6b80
esseya <eseasm@gmail.com>
2024-05-20 15:50:24 +0200

    Add project files.

---
+++ anställdaLista/Program.cs
@@ -0,0 +1,24 @@
+﻿List<Anstalld> anstallda = new List<Anstalld>();
+Console.WriteLine("Enter antal anställda på restaurang"); 
+var antalanstallda = int.Parse(Console.ReadLine());
+for (int i = 0; i < antalanstallda; i++)
+{
+    Console.WriteLine($"Enter information for person {i + 1}");
+    Console.Write("Namn: ");
+    var name = Console.ReadLine();
+    Console.WriteLine("Lön: ");
+    var lon = decimal.Parse(Console.ReadLine());
+    anstallda.Add(new Anstalld { Namn = name, Lon = lon });
+}
+Console.WriteLine("anställda Lista");
+foreach (var anstalld in anstallda)
+{
+    Console.WriteLine($"Namn: {anstalld.Namn}, Lön: {anstalld.Lon}");
+}
+
+
+internal class Anstalld
+{
+    public string ?Namn { get; set; }
+    public decimal Lon { get; set; }
+}
